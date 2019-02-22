afdcc870725a846862636bbeda11fa877fe58573
Abhinish Raj <abhinish.raj13@gmail.com>
2019-02-22 18:26:43 +0530

    Url Updated #4

--- src/apicall.js
+++ src/apicall.js
@@ -28,9 +28,8 @@ var callAPI = {
     var id = storageAPI.get('id');
     console.log(id);
     xhttp.open("GET", `http://${apiHost}:${apiPort}/setScore/${id}/${score}`, true);
-    xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhttp.setRequestHeader("Access-Control-Allow-Origin", "*.apps.d94a.example.opentlc.com");
     xhttp.setRequestHeader("Access-Control-Allow-Methods","GET, POST, PATCH, PUT, DELETE, OPTIONS");
-    xhttp.setRequestHeader("Access-Control-Request-Headers","*");
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
     
