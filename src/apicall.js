137807ea088351055ae53ad2087c798a4087e9be
Abhinish13 <abhinish.gecr@gmail.com>
2019-03-30 07:59:37 +0530

    URL update #19

--- src/apicall.js
+++ src/apicall.js
@@ -1,59 +1,58 @@
-
 // if (!process.env.API_SERVICE_NAME) {
 //     var apiServiceName = process.env.API_SERVICE_NAME.toUpperCase() || 'crome',
 //         apiHost = process.env[apiServiceName + '_SERVICE_HOST'],
 //         apiPort = process.env[apiServiceName + '_SERVICE_PORT'];
 // } else
 // {
- var apiHost = 'candygameapi.apps.d94a.example.opentlc.com';
+//  var apiHost = 'candygameapi.apps.d94a.example.opentlc.com';
+var apiHost = 'candygameapi-candy.apps.prod.pdcloudex.com',
     apiPort = 80;
 
 
 //console.log(apiServiceName+'\n'+apiHost+'\n'+apiPort);
 var callAPI = {
 
-    loadDoc :function()
-{
-    var xhttp = new XMLHttpRequest();
+    loadDoc: function () {
+        var xhttp = new XMLHttpRequest();
 
-    xhttp.open("GET", `http://${apiHost}:${apiPort}/leaderboard`, true);
-    xhttp.send();
-    // console.log(xhttp.toString());
-},
+        xhttp.open("GET", `http://${apiHost}:${apiPort}/leaderboard`, true);
+        xhttp.send();
+        // console.log(xhttp.toString());
+    },
 
 
-    setScore :function (score) {
-    var xhttp = new XMLHttpRequest();
-    var params = null;
+    setScore: function (score) {
+        var xhttp = new XMLHttpRequest();
+        var params = null;
 
-    var id = storageAPI.get('id');
-    console.log(id);
-    xhttp.open("GET", `http://${apiHost}:${apiPort}/setScore/${id}/${score}`, true);
-    xhttp.setRequestHeader("Access-Control-Allow-Origin", "http://candygameapi.apps.d94a.example.opentlc.com");
-    xhttp.setRequestHeader("Access-Control-Allow-Methods","GET, POST, PATCH, PUT, DELETE, OPTIONS");
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.setRequestHeader("cache-control", "no-cache");
-    xhttp.send(params);
-    
-    console.log(xhttp.getAllResponseHeaders());
-},
+        var id = storageAPI.get('id');
+        console.log(id);
+        xhttp.open("GET", `http://${apiHost}:${apiPort}/setScore/${id}/${score}`, true);
+        xhttp.setRequestHeader("Access-Control-Allow-Origin", "http://candygameapi.apps.d94a.example.opentlc.com");
+        xhttp.setRequestHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.setRequestHeader("cache-control", "no-cache");
+        xhttp.send(params);
+
+        console.log(xhttp.getAllResponseHeaders());
+    },
     createPlayer: function () {
-    var xhttp = new XMLHttpRequest();
+        var xhttp = new XMLHttpRequest();
 
-    var name = callAPI.requestName();
+        var name = callAPI.requestName();
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200){
-            console.log(this.responseText);
-            storageAPI.set('id',this.responseText);
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+                storageAPI.set('id', this.responseText);
+            }
         }
-    }
 
-    xhttp.open("GET", `http://${apiHost}:${apiPort}/player/${name}`, true);
-    xhttp.send();
+        xhttp.open("GET", `http://${apiHost}:${apiPort}/player/${name}`, true);
+        xhttp.send();
 
     },
-    requestName : function myFunction() {
+    requestName: function myFunction() {
         var txt;
         var person = prompt("Please enter your Email Address:", "Email Address");
         if (person == null || person == "") {
@@ -64,7 +63,7 @@ var callAPI = {
         // document.getElementById("demo").innerHTML = txt;
         return person;
     },
-    removeId : function() {
+    removeId: function () {
         storageAPI.remove('id');
     }
 };
